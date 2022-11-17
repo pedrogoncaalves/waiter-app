0f54d3144bd156a2b1d738abdf2f4edfe819e469
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2022-11-16 23:40:01 -0300

    styles

--- waiter-app/src/App.tsx
+++ waiter-app/src/App.tsx
@@ -2,12 +2,15 @@ import { GlobalStyles } from "./styles/GlobalStyle"
 
 import { Header } from "./components/Header";
 
+import { Orders } from "./components/Orders";
+
 
 export const App = () => {
   return(
     <>
     <GlobalStyles/>
     <Header/>
+    <Orders/>
     </>
    
   )
---
+++ waiter-app/src/components/Orders/index.tsx
@@ -0,0 +1,53 @@
+import * as S from './styles'
+
+export const Orders = () => {
+    return(
+        <S.Container>
+        <S.Board>
+            <header>
+                <span>🕑</span>
+                <strong>Fila de Espera</strong>
+                <span>(1)</span>
+            </header>
+
+            <S.orderContainer>
+                <button type="button">
+                    <strong>Mesa 2</strong>
+                    <span> 2 itens</span>
+
+                </button>
+            </S.orderContainer>
+        </S.Board>
+        <S.Board>
+            <header>
+                <span>🕑</span>
+                <strong>Fila de Espera</strong>
+                <span>(1)</span>
+            </header>
+
+            <S.orderContainer>
+                <button type="button">
+                    <strong>Mesa 2</strong>
+                    <span> 2 itens</span>
+
+                </button>
+            </S.orderContainer>
+        </S.Board>
+        <S.Board>
+            <header>
+                <span>🕑</span>
+                <strong>Fila de Espera</strong>
+                <span>(1)</span>
+            </header>
+
+            <S.orderContainer>
+                <button type="button">
+                    <strong>Mesa 2</strong>
+                    <span> 2 itens</span>
+
+                </button>
+            </S.orderContainer>
+        </S.Board>
+        </S.Container>
+    )
+}
