e5b69edcf5028f7928b1f02fa5346f9ce74b12fd
Pedro Gonçalves <101778028+pedrogoncaalves@users.noreply.github.com>
2022-11-29 17:46:19 -0300

    finalizing

--- waiter-app/src/components/OrderModal/index.tsx
+++ waiter-app/src/components/OrderModal/index.tsx
@@ -3,6 +3,7 @@ import React, { useState} from "react";
 import * as C from './styles'
 import { Order } from '../../types/Order'
 import closeIcon from '../../assets/images/close-icon.svg'
+import { formatCurrency } from '../../utils/formatCurrency'
 
 interface OrderModalProps {
     visible: boolean;
@@ -45,6 +46,29 @@ export const OrderModal = ({visible, order}: OrderModalProps) => {
             </div>
             <C.OrderDetails>
                 <strong>Itens</strong>
+
+                <div className="order-items">
+                {order.products.map(({_id, product, quantity}) => (
+                    <div className="item" key={_id}>
+                        <img 
+                        src={`http://localhost:3001/uploads/${product.imagePath}`}
+                        alt={product.name}
+                        width="56"
+                        height="28.51"/>
+                        <span className="quantity">{quantity}x</span>
+
+                        <div className="product-details">
+                            <strong>{product.name}</strong>
+                            <span>{formatCurrency(product.price)}</span>
+                        </div>
+                        
+                    </div>
+                ))}
+                </div>
+                <div className="total">
+                    <span>Total</span>
+                    <strong>{formatCurrency(</strong>
+                </div>
             </C.OrderDetails>
         </C.ModalBody>
         </C.Overlay>
